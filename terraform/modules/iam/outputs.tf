f92b890208935717db6d536ecd9add192738f8c9
sumit05 <sumit884042@gmail.com>
2026-08-02 22:42:43 +0530

    this is the project which i am going to build from scratch

--- terraform/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "eks_cluster_role_arn" {
-  description = "The ARN of the IAM role for the EKS cluster"
-  value = aws_iam_role.eks_cluster.arn
-}
-
-output "eks_node_group_role_arn" {
-  description = "The ARN of the IAM role for the EKS node group"
-  value = aws_iam_role.eks_node_group.arn
-}
--- terraform/modules/rds/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "db_endpoint" {
-  description = "The endpoint of the RDS database"
-  value = aws_db_instance.main.address
-}
-output "db_port" {
-  description = "The port of the RDS database"
-  value = aws_db_instance.main.port
-}
