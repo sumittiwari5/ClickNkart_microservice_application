f80c9fe00c03fe6fe316806dd138fa27b3425a70
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-08-10 15:57:30 +0530

    Remove EKS security group outputs from outputs.tf
    Removed outputs for EKS cluster and nodes security groups.

--- terraform/modules/security-groups/outputs.tf
+++ terraform/modules/security-groups/outputs.tf
@@ -1,11 +1,3 @@
 output "jumpserver_security_group_id" {
   value = aws_security_group.jumpserver.id
 }
-
-output "eks_cluster_security_group_id" {
-  value = aws_security_group.eks_cluster.id
-}
-
-output "eks_nodes_security_group_id" {
-  value = aws_security_group.eks_nodes.id
-}
