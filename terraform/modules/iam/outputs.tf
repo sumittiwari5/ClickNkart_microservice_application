2c476449d521027cd3daf1ea0de308d9fadbb006
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-09-07 00:25:09 +0530

    Add output for AWS Load Balancer Controller role ARN

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -12,3 +12,8 @@ output "jumpserver_instance_profile_name" {
   description = "Instance profile name for the Jump Server"
   value       = aws_iam_instance_profile.jumpserver.name
 }
+
+output "aws_load_balancer_controller_role_arn" {
+  description = "IAM role ARN for AWS Load Balancer Controller"
+  value       = aws_iam_role.aws_load_balancer_controller.arn
+}
