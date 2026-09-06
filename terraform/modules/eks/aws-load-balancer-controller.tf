2fcee02910f770b95d068942195be49262aa4728
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-09-07 01:10:12 +0530

    Add AWS Load Balancer Controller Helm release

--- terraform/modules/eks/aws-load-balancer-controller.tf
+++ terraform/modules/eks/aws-load-balancer-controller.tf
@@ -13,3 +13,39 @@ resource "kubernetes_service_account_v1" "aws_load_balancer_controller" {
     }
   }
 }
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "3.5.0"
+
+  namespace = "kube-system"
+
+  set = [
+    {
+      name  = "clusterName"
+      value = var.cluster_name
+    },
+    {
+      name  = "region"
+      value = data.aws_region.current.name
+    },
+    {
+      name  = "vpcId"
+      value = var.vpc_id
+    },
+    {
+      name  = "serviceAccount.create"
+      value = "false"
+    },
+    {
+      name  = "serviceAccount.name"
+      value = "aws-load-balancer-controller"
+    }
+  ]
+
+  depends_on = [
+    kubernetes_service_account_v1.aws_load_balancer_controller
+  ]
+}
