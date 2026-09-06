8ecd5d3827f7e9ef545b04865e3d6b0907f9d304
Sumit Tiwari <134449481+sumittiwari5@users.noreply.github.com>
2026-09-07 00:18:37 +0530

    update variable.tf

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -12,3 +12,8 @@ variable "eks_oidc_provider_arn" {
   description = "ARN of the EKS OIDC provider used for IRSA"
   type        = string
 }
+
+variable "eks_oidc_issuer_url" {
+  description = "OIDC issuer URL of the EKS cluster"
+  type        = string
+}
